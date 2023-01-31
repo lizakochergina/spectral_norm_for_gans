3cb2b67e00b7325198139d264448be3ec63c335d
Elizaveta Kochergina <el.kocherginaa@gmail.com>
2023-01-31 22:53:41 +0300

    fir model save

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ for epoch in tqdm(range(NUM_EPOCHS)):
     gen_model.scheduler_step()
 
     if epoch % SAVE_TIME == 0:
-        torch.save(gen_model.generator, 'models/generator_' + str(epoch) + '.pth')
         chi = evaluate_model(gen_model, features_test, data_test, losses_train, losses_test, len(data_train), chi_metric, epoch)
         chi_metric.append(chi)
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': gen_model.generator.state_dict(),
+            'metric': chi,
+        }, 'models/generator_' + str(epoch) + '.pth')
